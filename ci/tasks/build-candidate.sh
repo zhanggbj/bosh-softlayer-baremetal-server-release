b60f8ae627c462452f80499a6fb89eab4d580e42
Gong Zhang <zhanggbj@cn.ibm.com>
2016-06-07 16:59:56 +0800

    debug create

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -11,6 +11,9 @@ wget https://github.com/xcat2/xcat-core/releases/download/2.11_release/xcat-core
 wget https://github.com/xcat2/xcat-core/releases/download/2.11_release/xcat-dep-ubuntu-2.11.tar.bz -P blobs/xcat/
 wget http://sourceforge.net/projects/xcat/files/yum/devel/core-snap/xCAT-SoftLayer-2.10-snap201507240527.noarch.rpm/download -O xCAT-SoftLayer-2.10-snap201507240527.noarch.rpm -P blobs/xcat/
 
+ls blobs/xcat
+cat packages/xcat/spec
+
 pushd baremetal-server-release
 
 #  source .envrc
