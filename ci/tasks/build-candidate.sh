e36760e67e075dba5435a847c0a0db6af19421f0
Gong Zhang <zhanggbj@cn.ibm.com>
2016-06-08 14:02:31 +0800

    setup bps

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -5,26 +5,26 @@ semver=`cat version-semver/number`
 
 echo "pwd is "$PWD
 
-pushd baremetal-server-release
-  mkdir -p blobs/xcat/
-  wget https://github.com/xcat2/xcat-core/releases/download/2.11_release/xcat-core-2.11-ubuntu.tar.bz2 -P blobs/xcat/
-  wget https://github.com/xcat2/xcat-core/releases/download/2.11_release/xcat-dep-ubuntu-2.11.tar.bz -P blobs/xcat/
-  wget http://sourceforge.net/projects/xcat/files/yum/devel/core-snap/xCAT-SoftLayer-2.10-snap201507240527.noarch.rpm/download -O xCAT-SoftLayer-2.10-snap201507240527.noarch.rpm -P blobs/xcat/
-
-  git submodule update --init --recursive --force
-
-  echo "installing bosh CLI"
-  gem install bosh_cli --no-ri --no-rdoc
-
-  echo "using bosh CLI version..."
-  bosh version
-
-  bms_release_name="baremetal-server-dev-release"
-
-  cat packages/xcat/spec
-  echo "building baremetal server dev release..."
-  bosh create release --name $bms_release_name --version $semver --with-tarball --force
-
-popd
-
-mv baremetal-server-release/dev_releases/$bms_release_name/$bms_release_name-$semver.tgz candidate/
+#pushd baremetal-server-release
+#  mkdir -p blobs/xcat/
+#  wget https://github.com/xcat2/xcat-core/releases/download/2.11_release/xcat-core-2.11-ubuntu.tar.bz2 -P blobs/xcat/
+#  wget https://github.com/xcat2/xcat-core/releases/download/2.11_release/xcat-dep-ubuntu-2.11.tar.bz -P blobs/xcat/
+#  wget http://sourceforge.net/projects/xcat/files/yum/devel/core-snap/xCAT-SoftLayer-2.10-snap201507240527.noarch.rpm/download -O xCAT-SoftLayer-2.10-snap201507240527.noarch.rpm -P blobs/xcat/
+#
+#  git submodule update --init --recursive --force
+#
+#  echo "installing bosh CLI"
+#  gem install bosh_cli --no-ri --no-rdoc
+#
+#  echo "using bosh CLI version..."
+#  bosh version
+#
+#  bms_release_name="baremetal-server-dev-release"
+#
+#  cat packages/xcat/spec
+#  echo "building baremetal server dev release..."
+#  bosh create release --name $bms_release_name --version $semver --with-tarball --force
+#
+#popd
+#
+#mv baremetal-server-release/dev_releases/$bms_release_name/$bms_release_name-$semver.tgz candidate/
