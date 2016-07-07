276857d636557d564a0a5aa6dfeb85c6ef82144d
Gong Zhang <zhanggbj@cn.ibm.com>
2016-07-07 16:36:03 +0800

    debug upload

--- ci/tasks/verify-baremetal-server.sh
+++ ci/tasks/verify-baremetal-server.sh
@@ -2,6 +2,16 @@
 
 set -e -x
 
+function verify_return_val() {
+    local cmd=$1
+    local ret=$2
+    local expt=$3
+    if [ $ret -ne $expt ]; then
+       echo "command bmp" $cmd "failed"
+       exit 1
+    fi
+}
+
 BPS=`cat ${PWD}/baremetal-server-deployment/baremetal-server-info`
 DIRECTOR=`cat ${PWD}/deployment/director-info | awk "NR==1"`
 data_center=$SL_DATACENTER
@@ -40,14 +50,24 @@ mv bosh-softlayer-tools/bmp /usr/local/bin
 echo "{}" > $HOME/.bmp_config
 export NON_VERBOSE=true
 bmp target -t http://$BPS:8080
+verify_return_val "target" $? 0
 bmp login -u admin -p admin
+verify_return_val "login" $? 0
 bmp status
+verify_return_val "status" $? 0
 bmp bms
+verify_return_val "bms" $? 0
 bmp stemcells
+verify_return_val "stemcells" $? 0
 bmp create-baremetals --dryrun -d $deployment_file
+verify_return_val "create-baremetals" $? 0
 bmp tasks
+verify_return_val "tasks" $? 0
 bmp task --task_id=1
+verify_return_val "task --task_id" $? 0
 bmp sl --packages
+verify_return_val "sl --packages" $? 0
 bmp sl --package-options=255
+verify_return_val "sl --package-options" $? 0
 
 
