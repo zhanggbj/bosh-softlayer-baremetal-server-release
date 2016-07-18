d7c00130acccf5c9032bfac58244cad980540aa4
Gong Zhang <zhanggbj@cn.ibm.com>
2016-07-18 17:33:23 +0800

    debug

--- ci/tasks/config-baremetal-server.sh
+++ ci/tasks/config-baremetal-server.sh
@@ -3,6 +3,8 @@ set -e -x
 
 # Config bosh with bmp server
 #publicIp=`bosh vms|grep 'bps-pipeline/0' | awk '{print $11}'`
+bosh -n target ${BM_DIRECTOR_IP}
+bosh login admin admin
 publicIp=`bosh vms|grep 'a6f5fcd2-6b3e-4272-88a8-300d16f31dbb' | awk '{print $11}'`
 url='https://'"${SL_USERNAME}"':'"${SL_API_KEY}"'@api.softlayer.com/rest/v3/SoftLayer_Account/getVirtualGuests?objectMask=mask[primaryBackendIpAddress]&objectFilter={"virtualGuests":{"primaryIpAddress":{"operation":"'"${publicIp}"'"}}}'
 result=`curl -g $url`
