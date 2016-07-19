61ae6a6d17bb4cc1f01329c2847e6b697f585d75
Gong Zhang <zhanggbj@cn.ibm.com>
2016-07-19 11:04:00 +0800

    debug config bps

--- ci/tasks/config-baremetal-server.sh
+++ ci/tasks/config-baremetal-server.sh
@@ -2,10 +2,12 @@
 set -e -x
 
 # Config bosh with bmp server
-#publicIp=`bosh vms|grep 'bps-pipeline/0' | awk '{print $11}'`
+echo "login director..."
 bosh -n target ${BM_DIRECTOR_IP}
 bosh login admin admin
+#publicIp=`bosh vms|grep 'bps-pipeline/0' | awk '{print $11}'`
 publicIp=`bosh vms|grep 'a6f5fcd2-6b3e-4272-88a8-300d16f31dbb' | awk '{print $11}'`
+echo "public ip is" $publicIp
 url='https://'"${SL_USERNAME}"':'"${SL_API_KEY}"'@api.softlayer.com/rest/v3/SoftLayer_Account/getVirtualGuests?objectMask=mask[primaryBackendIpAddress]&objectFilter={"virtualGuests":{"primaryIpAddress":{"operation":"'"${publicIp}"'"}}}'
 result=`curl -g $url`
 privateIp=`grep -oP "primaryIpAddress.*?}}]" $result | grep -o "[0-9]*"|awk "NR==1"`
