16dd0e9b35ab3c323742b95f19066a4feb024dfe
Gong Zhang <zhanggbj@cn.ibm.com>
2016-08-09 20:14:42 +0800

    final debug

--- ci/tasks/cleanup.sh
+++ ci/tasks/cleanup.sh
@@ -16,18 +16,6 @@ echo "delete dummy-deployment"
 dummy_deployment="dummy-bm-pipeline"
 echo "yes" | bosh delete deployment $dummy_deployment --force
 
-echo "update baremetal state in pool"
-tar -zxvf bosh-softlayer-tools/bosh-softlayer-tools-*.tgz
-mv bmp /usr/local/bin
-echo "{}" > $HOME/.bmp_config
-export NON_VERBOSE=true
-bmp_server_info="${PWD}/bps-deployment/bmp-server-info"
-bmp_server=`cat ${bmp_server_info} | sed -n '1p'`
-bmp target -t http://$bmp_server:8080
-bmp login -u admin -p admin
-server_id="290736"
-echo "yes" | bmp update-state --server $server_id --state=bm.state.new
-
 echo "delete bps-deployment"
 bps_deployment="bps-pipeline"
 echo "yes" | bosh delete deployment $bps_deployment --force
