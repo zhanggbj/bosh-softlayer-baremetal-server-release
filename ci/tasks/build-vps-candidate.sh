ed7c73ddee3aa9df9847df7948716deefaa9bacd
Gong Zhang <zhanggbj@cn.ibm.com>
2016-11-25 10:35:10 +0800

    initialize version number

--- ci/tasks/build-vps-candidate.sh
+++ ci/tasks/build-vps-candidate.sh
@@ -9,16 +9,6 @@ pushd vps-release
   pushd blobs/golang_1.7/
     wget https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
   popd
-
-# for debug, remove other jobs and packages
-  rm -rf jobs/redis
-  rm -rf jobs/baremetal-provision-server
-  rm -rf jobs/xcat-server
-  rm -rf packages/baremetal-provision-server
-  rm -rf packages/libyaml
-  rm -rf packages/redis
-  rm -rf packages/ruby-2.2
-  rm -rf packages/xcat
   
   echo "installing bosh CLI"
   gem install bosh_cli --no-ri --no-rdo c
