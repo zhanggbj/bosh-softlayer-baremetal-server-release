fcea6b9275aa6ce260bfc543c4269410e589ef4b
Gong Zhang <zhanggbj@cn.ibm.com>
2016-11-30 17:19:37 +0800

    Add vps release pipeline
    This pipeline is to generate vps release.
    
    [#133573407] (https://www.pivotaltracker.com/story/show/133573407)

---
+++ ci/tasks/build-vps-candidate.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+semver=`cat version-semver/number`
+
+pushd vps-release
+
+  mkdir -p blobs/golang_1.7/
+  pushd blobs/golang_1.7/
+    wget https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
+  popd
+  
+  echo "installing bosh CLI"
+  gem install bosh_cli --no-ri --no-rdo c
+
+  echo "using bosh CLI version..."
+  bosh version
+
+  vps_release_name="vps"
+
+
+  echo "building baremetal server dev release..."
+  bosh create release --name $vps_release_name --version $semver --with-tarball --force
+popd
+
+mv vps-release/dev_releases/$vps_release_name/$vps_release_name-$semver.tgz candidate/
