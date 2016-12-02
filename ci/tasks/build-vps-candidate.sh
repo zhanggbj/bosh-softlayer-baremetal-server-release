28b2edc911ced45fd90864440f2c042e4ac48fde
Gong Zhang <zhanggbj@cn.ibm.com>
2016-12-02 18:36:19 +0800

    changed release name

--- ci/tasks/build-vps-candidate.sh
+++ ci/tasks/build-vps-candidate.sh
@@ -9,15 +9,14 @@ pushd vps-release
   pushd blobs/golang_1.7/
     wget https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz
   popd
-  
+
   echo "installing bosh CLI"
   gem install bosh_cli --no-ri --no-rdo c
 
   echo "using bosh CLI version..."
   bosh version
 
-  vps_release_name="vps"
-
+  vps_release_name="bosh-softlayer-pool-server"
 
   echo "building baremetal server dev release..."
   bosh create release --name $vps_release_name --version $semver --with-tarball --force
